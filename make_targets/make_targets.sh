535ad87a8815e83f48759ab22088d6f70989b413
reberp <my@email.com>
2023-10-10 14:34:29 -0500

    first

---
+++ make_targets/make_targets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#lil helper for making a few containers that I can run ansible against
+echo "Creating $1 targets"
+for ((i = 1 ; i <= $1; i++));
+do
+  # echo "$i"
+  container_id=$(docker run -it -d ubuntu_ssh)
+  ip=$(docker inspect -f'{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
+  echo "target$i ansible_host=$ip ansible_ssh_pass=pat"
+done
